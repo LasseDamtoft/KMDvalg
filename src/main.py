3351d46ec5cb4a05bf24900c8a585a67d548e72c
Lasse <lasse-damtoft@hotmail.com>
2022-07-28 20:05:12 +0200

    Refactor to utilize multiprocessing and save to csv

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from loginit import LogInitializer
 from kmdvalg import KMDValg
+from loginit import LogInitializer
 
 if __name__ == '__main__':
     logger = LogInitializer().logger
-    kmd_scraper = KMDValg(logger)
-    kmd_scraper.run()
+    kmd_scraper = KMDValg(logger, processes=12, region_url='R84712')
+    kmd_scraper.scrape_to_csv()
